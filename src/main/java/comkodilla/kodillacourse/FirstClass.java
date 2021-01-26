a55cd4a398ab45fece9caf03e5a915a9f829401d
Jakub Piejko <https://github.com/Kubeus14/Jakub-Piejko-kodilla-java.git>
2021-01-26 16:34:20 +0100

    ownProject

---
+++ src/main/java/comkodilla/kodillacourse/FirstClass.java
@@ -0,0 +1,4 @@
+package comkodilla.kodillacourse;
+
+public class FirstClass {
+}
---
+++ src/test/java/comkodilla/kodillacourse/KodillaCourseOwnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package comkodilla.kodillacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaCourseOwnProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
